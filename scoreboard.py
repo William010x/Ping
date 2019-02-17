066065063a345b07106f11b57d101f70cb4dcfcd
Jennifer Huynh <jenniferr.huynh@mail.utoronto.ca>
2019-02-17 18:26:37 -0500

    Create scoreboard class
    Initialize variables player1 and player2 scores, make getters for the two variables, and write method headers for add_point and check_for_winner

---
+++ scoreboard.py
@@ -0,0 +1,23 @@
+class ScoreBoard:
+    """ScoreBoard class that tracks and updates two players' scores"""
+
+    def __init__(self):
+        """Creates a new instance of ScoreBoard"""
+        self.p1_score = 0
+        self.p2_score = 0
+        
+    def add_point(self, player):
+        """Increases player's score by 1 point"""
+        #TODO: add_point method
+        
+    def get_p1_score(self):
+        """Returns self.p1_score"""
+        return self.p1_score
+
+    def get_p2_score(self):
+        """Returns self.p2_score"""
+        return self.p2_score
+
+    def check_for_winner(self):
+        #TODO: method docstrings
+        #TODO: check_for_winner_method
