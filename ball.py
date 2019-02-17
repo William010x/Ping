a17a1602a355da4ad66332406291f4fdef8ccb76
William Hirsch <wigsinator@gmail.com>
2019-02-17 12:52:10 -0500

    Created Ball class framework and initializer
    In this commit I created the framework we're going to start with for the ball class, with an initializer for the 4 major variables the ball needs: an x and y position, and an x and y velocity.
    
    I also created placeholder methods for launch, which starts moving the ball at the start of a round, and bounce, which will run when the ball collides with another object.
    
    Pygame implementation is not included yet, and will be done at a later time.

---
+++ ball.py
@@ -0,0 +1,16 @@
+class Ball(object):
+	"""docstring for Ball"""
+	#TODO: Finish docstring for Ball class
+	def __init__(self, x_pos, y_pos, x_vel, y_vel):
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.x_vel = x_vel
+		self.y_vel = y_vel
+
+	def launch(self, direction):
+		#TODO: lanuch method 
+		pass
+
+	def bounce(self):
+		#TODO: bounce method
+		pass
