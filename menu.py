33828c1bade04137bc3fa069a5824fb37b451af8
Nooria Junaid <nooria.junaid@mail.utoronto.ca>
2019-02-17 19:36:27 -0500

    Implementation of display_menu in Menu Class
    This commit includes the implementation of display_menu within the Menu
    Class which allows the user to view the menu objects
    
    This commit also includes additional commented code (at the end of the
    file) to view the menu design while working on interactivity of objects
    in the class

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+import pygame
+import sys
+
 class Menu:
     '''Menu class that creates a menu and lists interactive menu options'''
     
@@ -10,7 +13,36 @@ class Menu:
         
     def display_menu(self):
         '''Displays menu objects on user's screen'''
-        #TODO: Finish implementation of display_menu
+        #TODO: Implement hover on text and calls when an option is selected.
+        
+        pygame.init()
+        font_colour = (255, 255, 255)
+        screen = pygame.display.set_mode(self.screen_resolution)
+        
+        #Create the text to be displayed with the desired game font.
+        title_font = pygame.font.Font("resources/fonts/3Dventure.ttf", 60)
+        menu_font = pygame.font.Font("resources/fonts/3Dventure.ttf", 20)
+        title = title_font.render("PING!", False, font_colour)
+        start_option = menu_font.render("Start", False, font_colour)
+        settings_option = menu_font.render("Settings", False, font_colour)
+        quit_option = menu_font.render("Quit", False, font_colour)
+        
+        #Begin displaying the menu.
+        menu_display = True
+        while menu_display:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    menu_display = False
+                    pygame.quit()
+                    sys.exit()
+            pygame.event.pump()
+            screen.fill((0, 0, 0))
+            screen.blit(title, (120, 80))
+            screen.blit(start_option, (150, 150))
+            screen.blit(settings_option, (150, 170))
+            screen.blit(quit_option, (150, 190))            
+            
+            pygame.display.flip()        
         
     def start_game(self):
         '''Starts the game when called by user'''
@@ -25,3 +57,7 @@ class Menu:
     def quit_game(self):
         '''Exits user from the game'''
         #TODO: Finish implementation of quit_game
+        
+# Uncomment the following for in-class display of the menu.
+# check_menu_display = Menu()
+# check_menu_display.display_menu()
