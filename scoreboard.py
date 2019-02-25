46e209c6f23e8b23f648d64038aa223937989c6c
Jennifer Huynh <jenniferr.huynh@mail.utoronto.ca>
2019-02-25 18:24:07 -0500

    add check_for_winner method to ScoreBoard class

--- scoreboard.py
+++ scoreboard.py
@@ -1,14 +1,20 @@
+import pygame, sys
+
 class ScoreBoard:
     """ScoreBoard class that tracks and updates two players' scores"""
 
-    def __init__(self):
+    def __init__(self, display):
         """Creates a new instance of ScoreBoard"""
         self.p1_score = 0
         self.p2_score = 0
+        self.display = display
         
     def add_point(self, player):
         """Increases player's score by 1 point"""
-        #TODO: add_point method
+        if (player == "p1"):
+            self.p1_score += 1
+        else:
+            self.p2_score += 1
         
     def get_p1_score(self):
         """Returns self.p1_score"""
@@ -19,5 +25,12 @@ class ScoreBoard:
         return self.p2_score
 
     def check_for_winner(self):
-        #TODO: method docstrings
-        #TODO: check_for_winner_method
+        """Returns 'p1' if self.p1_score is 7
+           Returns 'p2' if self.p2_score is 7
+           Returns None otherwise
+        """
+        if (self.p1_score == 7):
+            return "p1"
+        elif (self.p2_score == 7):
+            return "p2"
+        return None
