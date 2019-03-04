fb3f2f219eebf7e87483f29846e9c84e844ece79
William San <william.san@mail.utoronto.ca>
2019-03-03 21:21:01 -0500

    Create Paddle class
    Add a Paddle class, which stores x and y coordinates. Coordinates
    represent location of the paddle's top-left corner. Variables are
    private, and should only be accessed by getter methods. Only y can
    be modified, through the move method.

---
+++ paddle.py
@@ -0,0 +1,14 @@
+class Paddle:
+    def __init__(self, x_pos, y_pos):
+        self.__x_pos = x_pos
+        self.__y_pos = y_pos
+        
+    def move(speed):
+        self.__y_pos += speed
+        
+    def get_x_pos():
+        return self.__x_pos
+    
+    def get_y_pos():
+        return self.__y_pos
+    
