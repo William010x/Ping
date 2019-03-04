540bf11c1801f074a2bb6426323c6948c7a4e68f
William San <william.san@mail.utoronto.ca>
2019-03-03 21:37:53 -0500

    Refactor main loop and add paddle movement
    Create method for main loop so game can be restarted. Add game clock to
    track time within the game. Fill in stubs for paddle movement events
    and framework to update board. Add constants for color and paddle speed.

--- ping.py
+++ ping.py
@@ -1,47 +1,56 @@
 import pygame, sys
-from pygame.locals import *
 
 pygame.init()
-DISPLAY = pygame.display.set_mode((400, 300))
+display = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Ping!')
+clock = pygame.time.Clock()
 
-crashed = False
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-########### EVENTS ###########
+PADDLE_SPEED = 5
 
-
-##############################
-
-while not crashed: # main game loop
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            crashed = True
-            pygame.quit()
-            sys.exit()
-        
-        ########### PADDLE MOVEMENT ###########
-        elif event.type == pygame.KEYDOWN:
-            pygame.quit()
-            if event.key == pygame.K_UP:
-                # TODO: Move paddle 2 up
-                pass
-            elif event.key == pygame.K_DOWN:
-                # TODO: Move paddle 2 down
-                pass
-            elif event.key == pygame.K_w:
-                # TODO: Move paddle 1 up
-                pass
-            elif event.key == pygame.K_s:
-                # TODO: Move paddle 1 down
-                pass
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                # TODO: Stop moving paddle 2
-                pass
-            elif event.key == pygame.K_w or event.key == pygame.K_s:
-                # TODO: Stop moving paddle 1
-                pass
-        ########### PADDLE MOVEMENT ###########
-        
-    pygame.display.update()
+def game_loop():
+    # Initialize game data
+    # board = Board()
+    paddle_1_change = 0
+    paddle_2_change = 0
+    crashed = False
     
+    while not crashed: # main game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+                pygame.quit()
+                sys.exit()
+                
+            ########### PADDLE MOVEMENT ###########
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    paddle_2_change = -PADDLE_SPEED
+                elif event.key == pygame.K_DOWN:
+                    paddle_2_change = PADDLE_SPEED
+                elif event.key == pygame.K_w:
+                    paddle_1_change = -PADDLE_SPEED
+                elif event.key == pygame.K_s:
+                    paddle_1_change = PADDLE_SPEED
+                    
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    paddle_2_change = 0
+                elif event.key == pygame.K_w or event.key == pygame.K_s:
+                    paddle_1_change = 0
+            ########### PADDLE MOVEMENT ###########
+            
+        # board.move_paddle_1(paddle_1_change)
+        # board.move_paddle_2(paddle_2_change)
+        # board.move_ball()
+        display.fill(BLACK)
+        # board.draw()
+        
+        pygame.display.update()
+        clock.tick(60)
+    
+game_loop()
+pygame.quit()
+sys.exit()
